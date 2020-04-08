7e91262fd5846f4cadb6e3bd372abf64a9a16b61
vinhvang89 <61720675+vinhvang89@users.noreply.github.com>
2020-04-08 19:40:51 +0700

    new

--- VeCacHinh/src/DrawShapes.java
+++ VeCacHinh/src/DrawShapes.java
@@ -9,6 +9,7 @@ public class DrawShapes {
         System.out.println("3 . Big nose");
         System.out.println("4 . Saboteur");
         System.out.println("5 . Rubbit");
+        System.out.println("anyNumber . Triangle down");
         System.out.println("0 . Exit");
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your choose :");
@@ -36,7 +37,14 @@ public class DrawShapes {
                 System.out.println("(!!)(!!)");
                 break;
             }
-        }
+            default:{
+                char a = '*';
+                for (int i = 6; i >0 ; i--) {
+                    System.out.println(i*a);
+                    }
+                break;
+                }
+            }
         while (choose != 0){
             System.out.println("Enter your choose :");
             choose = scan.nextInt();
@@ -63,6 +71,19 @@ public class DrawShapes {
                     System.out.println("(!!)(!!)");
                     break;
                 }
+                default:{
+                        String a = "*";
+                        for (int j = 0 ; j < 6 ; j++) {
+                            for (int i = 6; i > 0; i--) {
+                                a = a + "*";
+                            }
+                            System.out.println(a);
+                        }
+
+                        break;
+
+                }
+
             }
         }
     }
