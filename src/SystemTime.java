9ed0af7de83f9255e285c685cb09d890526500ef
vinhvang89 <61720675+vinhvang89@users.noreply.github.com>
2020-04-08 11:33:34 +0700

    new

--- src/SystemTime.java
+++
@@ -1,7 +0,0 @@
-import java.util.Date;
-public class Systemtime {
-    public static void main(String[] args) {
-        Date now = new Date();
-        System.out.println("bay gio la :" +now);
-    }
-}
