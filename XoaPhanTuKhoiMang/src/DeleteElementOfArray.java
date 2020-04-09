f1e9b2ffbb09515c4d259577db1d68fab8a6aeed
vinhvang89 <61720675+vinhvang89@users.noreply.github.com>
2020-04-09 13:05:27 +0700

    new

---
+++ XoaPhanTuKhoiMang/src/DeleteElementOfArray.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DeleteElementOfArray {
+    public static void main(String[] args) {
+            int[] array = {1,34,2,6,7,8,89,55};
+            int[] array1 = new int[8];
+            Scanner scan = new Scanner(System.in);
+            int a;
+            System.out.println("Enter element which you want to delete :");
+            a = scan.nextInt();
+            int j = 0;
+            for (int i = 0; i < array.length ; i++) {
+                if ( array[i] != a){
+                 array1[j] = array[i];
+                j++;
+                }
+
+            }
+            array = array1;
+
+        System.out.println(Arrays.toString(array));
+    }
+}
