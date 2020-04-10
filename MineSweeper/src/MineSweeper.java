ffeac74f70382696dd641c8f9bcc1b474d0755a9
vinhvang89 <61720675+vinhvang89@users.noreply.github.com>
2020-04-10 20:06:38 +0700

    remake

--- MineSweeper/src/MineSweeper.java
+++ MineSweeper/src/MineSweeper.java
@@ -3,28 +3,47 @@ import java.util.Arrays;
 
 public class MineSweeper {
     public static void main(String[] args) {
-        int[][] map = { {0,0,0,0,0},
-                        {0,0,33,0,0},
-                        {0,33,0,0,0},
-                        {0,0,0,33,0},
-                        {0,0,0,0,0}};
-
-//        Set up so quanh bomb
-        for (int i = 0; i < 5 ; i++) {
-            for (int j = 0;j<5;j++){
-                if(map[i][j] == 33){
-                   if(map[i+1][j] != 33) map[i+1][j] +=1;
-                   if(map[i-1][j] != 33) map[i-1][j] +=1;
-                   if(map[i+1][j+1] !=33) map[i+1][j+1] +=1;
-                   if(map[i-1][j-1] != 33) map[i-1][j-1] +=1;
-                   if(map[i][j+1] !=33) map[i][j+1] +=1;
-                   if(map[i][j-1] !=33) map[i][j-1] +=1;
-                   if(map[i-1][j+1] !=33) map[i-1][j+1] +=1;
-                   if (map[i+1][j-1] != 33) map[i+1][j-1] +=1;
+        String[][] map = {{"*",".",".",".","*"},
+                          {"*","*",".",".","."},
+                          {".",".","*",".","."},
+                          {".",".",".",".","."},
+                          {".",".",".",".","."}};
+        final int WIDTH = map.length;
+        int[][] report = {{0,0,0,0,0},
+                          {0,0,0,0,0},
+                          {0,0,0,0,0},
+                          {0,0,0,0,0},
+                          {0,0,0,0,0}} ;
+        for (int i = 0; i < WIDTH ; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                if (map[i][j].equals("*")) {
+                    report[i][j] = 10;
+                    if( i  >= 1 )
+                        report[i - 1][j] += 1;
+                    if( j  >= 1 && i >= 1)
+                        report[i - 1][j - 1] += 1;
+                    if(j - 1 >=0)
+                        report[i][j -1 ] +=1;
+                    if(i + 1 < WIDTH)
+                        report[i + 1][j] += 1;
+                    if(j + 1 < WIDTH)
+                        report[i][j + 1] +=1;
+                    if ( i + 1 < WIDTH && j + 1 < WIDTH)
+                        report[i + 1][ j + 1] += 1;
+                    if( j - 1 >= 0 && i + 1 < WIDTH )
+                    report[i + 1][ j -1] += 1;
+                    if ( i - 1 >= 0 && j  + 1 < WIDTH)
+                    report[i - 1][j + 1] += 1;
                 }
             }
         }
-
+        for (int i = 0; i < WIDTH ; i++){
+            for (int j = 0 ; j < WIDTH; j++){
+                if(!map[i][j].equals("*")){
+                    map[i][j] = String.valueOf(report[i][j]);
+                }
+            }
+        }
         System.out.println(Arrays.toString(map[0]));
         System.out.println(Arrays.toString(map[1]));
         System.out.println(Arrays.toString(map[2]));
