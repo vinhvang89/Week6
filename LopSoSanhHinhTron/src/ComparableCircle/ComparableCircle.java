2efd4bd084c79996de712cee0f0a9abc5625ef93
vinhvang89 <61720675+vinhvang89@users.noreply.github.com>
2020-04-12 07:49:32 +0700

    new

---
+++ LopSoSanhHinhTron/src/ComparableCircle/ComparableCircle.java
@@ -0,0 +1,22 @@
+package ComparableCircle;
+import javafx.scene.shape.Circle;
+public class ComparableCircle extends Circle implements Comparable<ComparableCircle> {
+    public ComparableCircle(){
+
+    }
+    public ComparableCircle(double radius){
+        super(radius);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if( getRadius() > o.getRadius())
+            return 1;
+        else if ( getRadius() < o.getRadius())
+            return  -1;
+        else
+            return 0;
+    }
+
+}
+
---
+++ LopSoSanhHinhTron/src/ComparableCircle/ComparableTest.java
@@ -0,0 +1,24 @@
+package ComparableCircle;
+
+import java.util.Arrays;
+
+public class ComparableTest {
+    public static void main(String[] args) {
+        ComparableCircle[] circles = new ComparableCircle[3];
+        circles[0] = new ComparableCircle(3.6);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle(3.5);
+
+        System.out.println("Pre-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+
+        Arrays.sort(circles);
+
+        System.out.println("After-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
